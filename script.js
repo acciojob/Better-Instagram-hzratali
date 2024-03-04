717a1047ef279db9a6f03f5a8e98640d5cf4a422
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-03-04 16:47:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
-function allowDrop(ev) {
-  console.log("allowDrop");
-  ev.preventDefault();
+let dragindex = 0;
+let dropindex = 0;
+let clone = "";
+const images = document.querySelectorAll(".image");
+function drag(e) {
+  e.dataTransfer.setData("text", e.target.id);
 }
-
-function drag(ev) {
-  console.log("drag");
-  ev.dataTransfer.setData("text", ev.target.id);
+function allowDrop(e) {
+  e.preventDefault();
 }
-
-function drop(ev) {
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
-  var target = ev.target;
-
-  // Ensure that the target is a div with the "image" class
-  if (target.classList.contains("image")) {
-    // Swap background images
-    console.log("Swapping images...");
-    var draggedImage = document.getElementById(data);
-    var tempBackground = target.style.backgroundImage;
-    target.style.backgroundImage = draggedImage.style.backgroundImage;
-    draggedImage.style.backgroundImage = tempBackground;
+function drop(e) {
+  clone = e.target.cloneNode(true);
+  let data = e.dataTransfer.getData("text");
+  let nodelist = document.getElementById("parent").childNodes;
+  console.log(data, e.target.id);
+  for (let i = 0; i < nodelist.length; i++) {
+    if (nodelist[i].id == data) {
+      dragindex = i;
+    }
   }
+  dragdrop(clone);
+  document
+    .getElementById("parent")
+    .replaceChild(document.getElementById(data), e.target);
+  document
+    .getElementById("parent")
+    .insertBefore(
+      clone,
+      document.getElementById("parent").childNodes[dragindex]
+    );
 }
+const dragdrop = (image) => {
+  image.ondragstart = drag;
+  image.ondragover = allowDrop;
+  image.ondrop = drop;
+};
+images.forEach(dragdrop);
